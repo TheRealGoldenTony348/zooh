837e402eaab824093050dc5a019950e283254318
TheRealGoldenTony348 <antoniwolski348@gmail.com>
2024-12-12 15:33:13 +0100

    Added Header (WIP)

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-
+import Header from "./components/Header";
+import "./App.css";
 
 function App() {
 
   return (
     <div>
+      <Header></Header>
       <h1>Title</h1>
     </div>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import "./Header.css";
+
+function Header() {
+    return(
+        <header>
+            <h1>Zooh!</h1>
+            <div>
+                <a href="/">Map</a>
+                <a href="/">Page 1</a>
+                <a href="/">Page 2</a>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
