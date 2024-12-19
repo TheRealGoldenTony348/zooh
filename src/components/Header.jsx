164a7503d8a68bc8817fbf24e5afd0a8cd6a89ec
TheRealGoldenTony348 <antoniwolski348@gmail.com>
2024-12-19 15:38:30 +0100

    added Card and minor changes to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,6 +9,7 @@ function Header() {
                 <Link className="pageLink" to="/">Home</Link>
                 <Link className="pageLink" to="/pageone">Page 1</Link>
             </nav>
+            <button className="menuButton">=</button>
         </header>
     );
 }
