00f61013cadd0e8933928f7075340c2c3315cf40
AstralNightshadow <phi.sutter@gmx.ch>
2025-01-23 15:56:37 +0100

    map added to map

--- src/pages/map.jsx
+++ src/pages/map.jsx
@@ -1,8 +1,9 @@
-
 function Map() {
-    return(
-        <div>map</div>
-    )
+    return (
+        <div style={{ height: '85vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4446.309146095922!2d8.572959649214578!3d47.385498129595405!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479aa0ea12b7a697%3A0x31929c3728d4b721!2sZoo%20Z%C3%BCrich!5e1!3m2!1sen!2sch!4v1737640921569!5m2!1sen!2sch" width="80%" height="100%" style={{ border: 0 }} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+    );
 }
 
 export default Map;
