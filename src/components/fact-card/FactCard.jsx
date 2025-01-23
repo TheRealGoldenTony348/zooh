9153ea308f8fd9ca2d68bbbc12d9c80ca83fcc62
TheRealGoldenTony348 <antoniwolski348@gmail.com>
2025-01-23 22:42:05 +0100

    added styling to Fact Card and page 2, added logic to Fact Card

--- src/components/fact-card/FactCard.jsx
+++ src/components/fact-card/FactCard.jsx
@@ -1,9 +1,11 @@
 import "./FactCard.css"
 
-function FactCard() {
-    return(
-        <div>
+function FactCard(props) {
+    let text = props.text;
 
+    return(
+        <div className="baseFactCard">
+            <h4>{text}</h4>
         </div>
     );
 }
--- src/pages/page2.jsx
+++ src/pages/page2.jsx
@@ -1,7 +1,21 @@
+import FactCard from "../components/fact-card/FactCard";
+import "./page2.css";
 
 function PageTwo() {
     return(
-        <div>page two</div>
+        <div className="basePageTwo">
+            <FactCard text={"Polar bears have black skin!"} />
+            <FactCard text={"Octopuses have three hearts!"} />
+
+            <FactCard text={"Girrafes pick their noses!"} />
+            <FactCard text={"Elephants can run pretty fast!"} />
+
+            <FactCard text={"Blue whales are immensely heavy!"} />
+            <FactCard text={"Koalas sleep almost all-day!"} />
+
+            <FactCard text={"Cows have best friends!"} />
+            <FactCard text={"Otters have the world's thickest coat!"} />
+        </div>
     )
 }
 
