cdae00a59fadb24ff9db5a790bb785226852f627
TheRealGoldenTony348 <antoniwolski348@gmail.com>
2025-01-23 21:24:34 +0100

    added Special offer and page1 styling

---
+++ src/components/special-offer/SpecialOffer.jsx
@@ -0,0 +1,13 @@
+import "./SpecialOffer.css"
+
+function SpecialOffer() {
+    return(
+        <div className="baseSpecialOffer">
+            <h2>Special Offers!</h2>
+            <h3>Special Offer 1</h3>
+            <h3>Special Offer 2</h3>
+        </div>
+    );
+}
+
+export default SpecialOffer;
--- src/pages/page1.jsx
+++ src/pages/page1.jsx
@@ -1,15 +1,17 @@
+import SpecialOffer from "../components/special-offer/SpecialOffer";
 import Ticket from "../components/ticket/Ticket";
+import "./page1.css"
 
 function PageOne() {
     return(
-        <div>
+        <div className="basePageOne">
             <div>
                 <Ticket title={"Ticket 1"} price={"10 CHF"} description={"default description 1"} />
                 <Ticket title={"Ticket 2"} price={"15 CHF"} description={"default description 2"} />
                 <Ticket title={"Ticket 3"} price={"20 CHF"} description={"default description 3"} />
                 <Ticket title={"Ticket 4"} price={"25 CHF"} description={"default description 4"} />
             </div>
-            
+            <SpecialOffer />
         </div>
     )
 }
